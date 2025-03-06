98b8e1202b3586296381e9fbd59901ec5d5fffb9
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-03-06 18:51:43 +0900

    [feat/#294] 좋아요한 기록 api 로직 구현

--- src/main/java/com/clokey/server/domain/history/application/HistoryServiceImpl.java
+++ src/main/java/com/clokey/server/domain/history/application/HistoryServiceImpl.java
@@ -422,6 +422,11 @@ public class HistoryServiceImpl implements HistoryService {
         hashtagToAdd.forEach(hashtag -> hashtagHistoryRepositoryService.addHashtagHistory(hashtag, history));
         hashtagToDelete.forEach(hashtag -> hashtagHistoryRepositoryService.deleteHashtagHistory(hashtag, history));
     }
-
-
+    
+    @Override
+    public HistoryResponseDTO.HistoryPreviewListResult getLikedHistories(Long memberId, int page) {
+        Page<History> histories = historyRepositoryService.findHistoriesByMemberIdAndMemberLike(memberId, PageRequest.of(page, 12));
+        Map<Long, String> historyImageMap = historyImageRepositoryService.findFirstImagesByHistoryIds(histories.stream().map(History::getId).toList());
+        return HistoryConverter.toHistoryPreviewListResult(histories, historyImageMap);
+    }
 }
