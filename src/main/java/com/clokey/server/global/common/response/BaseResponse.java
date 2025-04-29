b758718086ad5a371758591e635bf1fa6e8521ab
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-04-29 16:35:14 +0900

    [refactor/#351] src 변경 테스트

--- src/main/java/com/clokey/server/global/common/response/BaseResponse.java
+++ src/main/java/com/clokey/server/global/common/response/BaseResponse.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+//src 변경 테스트
 @Getter
 @AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
