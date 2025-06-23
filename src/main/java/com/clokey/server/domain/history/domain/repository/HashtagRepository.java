2cc79aab24de0583d572bbab39f8f15bd96c2fe4
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-06-23 19:30:01 +0900

    [feat/#355] HashtagRepository Test 작성 완료

--- src/main/java/com/clokey/server/domain/history/domain/repository/HashtagRepository.java
+++ src/main/java/com/clokey/server/domain/history/domain/repository/HashtagRepository.java
@@ -16,6 +16,7 @@ public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
     @Query("SELECT h FROM Hashtag h WHERE h.name IN :names")
     List<Hashtag> findHashtagsByNames(@Param("names") List<String> names);
 
+    // 테스트 코드 작성 안했음. -> 최적화 필요성 점검 필요함
     @Query("SELECT h.name FROM Hashtag h WHERE h.id NOT IN " +
             "(SELECT hh.hashtag.id FROM HashtagHistory hh JOIN hh.history h WHERE h.member.id = :memberId) " +
             "ORDER BY RAND() LIMIT 1")
---
+++ src/test/java/com/clokey/server/domain/history/domain/repository/HashtagRepositoryTest.java
@@ -0,0 +1,137 @@
+package com.clokey.server.domain.history.domain.repository;
+
+import com.clokey.server.domain.history.domain.entity.Hashtag;
+import com.clokey.server.domain.history.domain.entity.HashtagHistory;
+import com.clokey.server.domain.history.domain.entity.History;
+import com.clokey.server.domain.member.domain.entity.Member;
+import com.clokey.server.domain.member.domain.repository.MemberRepository;
+import com.clokey.server.domain.model.entity.enums.MemberStatus;
+import com.clokey.server.domain.model.entity.enums.SocialType;
+import com.clokey.server.domain.model.entity.enums.Visibility;
+import com.clokey.server.global.config.QuerydslConfig;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Import(QuerydslConfig.class)
+class HashtagRepositoryTest {
+
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private HistoryRepository historyRepository;
+    @Autowired private HashtagRepository hashtagRepository;
+    @Autowired private HashtagHistoryRepository hashtagHistoryRepository;
+
+    @BeforeAll
+    void setup() {
+
+
+        Member member1 = memberRepository.save(
+                Member.builder()
+                        .email("user1@example.com")
+                        .nickname("User1")
+                        .clokeyId("clokey1")
+                        .bio("안녕하세요, User1입니다.")
+                        .socialType(SocialType.APPLE)
+                        .profileImageUrl("https://example.com/user1.png")
+                        .status(MemberStatus.ACTIVE)
+                        .visibility(Visibility.PUBLIC)
+                        .build()
+        );
+
+        History history1 = historyRepository.save(
+                History.builder()
+                        .historyDate(LocalDate.of(2025, 1, 1))
+                        .likes(5)
+                        .visibility(Visibility.PUBLIC)
+                        .content("새해 첫 기록입니다.")
+                        .member(member1)
+                        .banned(false)
+                        .build()
+        );
+
+        History history2 = historyRepository.save(
+                History.builder()
+                        .historyDate(LocalDate.of(2025, 1, 2))
+                        .likes(2)
+                        .visibility(Visibility.PUBLIC)
+                        .content("여행을 다녀왔어요.")
+                        .member(member1)
+                        .banned(false)
+                        .build()
+        );
+
+        Hashtag tagSpring = hashtagRepository.save(
+                Hashtag.builder().name("봄").build()
+        );
+
+        Hashtag tagTravel = hashtagRepository.save(
+                Hashtag.builder().name("여행").build()
+        );
+
+        hashtagHistoryRepository.saveAll(List.of(
+                HashtagHistory.builder().hashtag(tagSpring).history(history1).build(),
+                HashtagHistory.builder().hashtag(tagTravel).history(history1).build(),
+                HashtagHistory.builder().hashtag(tagTravel).history(history2).build()
+        ));
+    }
+
+    @DisplayName("특정 해시태그를 이름을 기준으로 조회가 가능합니다.")
+    @Test
+    void 특정_해시태그_이름_기준_조회() {
+
+        // given & when
+        Optional<Hashtag> hashtag = hashtagRepository.findByName("봄");
+
+        // then
+        assertThat(hashtag.get().getName()).isEqualTo("봄");
+    }
+
+    @DisplayName("다수의 해시태그를 이름으로 조회할 수 있다.")
+    @Test
+    void 해시태그들_이름_기준_조회() {
+
+        // given & when
+        List<Hashtag> hashtags = hashtagRepository.findHashtagsByNames(List.of("봄","여행"));
+
+        // then
+        assertThat(hashtags.size()).isEqualTo(2);
+        assertThat(hashtags.get(0).getId()).isEqualTo(1L);
+        assertThat(hashtags.get(1).getId()).isEqualTo(2L);
+    }
+
+    @DisplayName("존재하는 해시태그들의 존재 여부를 확인할 수 있다.")
+    @ParameterizedTest(name = "hashtagName={0}, answer={1}")
+    @CsvSource(
+            value = {
+                    "봄,true",
+                    "여행,true",
+                    "없는 예시1,false",
+                    "없는 예시2,false"
+            }
+    )
+    void 해시태그_존재_여부_확인(String hashtagName, boolean answer) {
+
+        // given & when
+        boolean exists = hashtagRepository.existsByName(hashtagName);
+
+        // then
+        assertThat(exists).isEqualTo(answer);
+    }
+
+}
