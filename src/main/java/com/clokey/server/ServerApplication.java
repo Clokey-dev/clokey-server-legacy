605221cf143d0d40e0f21d82d531713ccc322376
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-07-02 01:05:58 +0900

    [feat/#369] JpaAuditingConfig 분리

--- src/main/java/com/clokey/server/ServerApplication.java
+++ src/main/java/com/clokey/server/ServerApplication.java
@@ -3,12 +3,10 @@ package com.clokey.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaAuditing
 @ConfigurationProperties("spring.data.redis")
 public class ServerApplication {
 
