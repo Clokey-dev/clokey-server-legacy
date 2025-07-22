4a76b47c0498f7bd3743b4142eee0f1ba02881db
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-07-22 23:15:34 +0900

    [chore/#372] 테스트 삭제 -> 2.0에서 작성될 예정

--- src/test/java/com/clokey/server/domain/WebMvcIntegrationTestSupport.java
+++
@@ -1,9 +0,0 @@
-package com.clokey.server.domain;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-@WebMvcTest(controllers = {
-        // 여기에 추가하세요
-})
-public abstract class WebMvcIntegrationTestSupport {
-}
